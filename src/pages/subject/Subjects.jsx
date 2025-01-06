d8010516795df69ee769e81ac551fd35f1df02aa
ApichaiTi <81065469+artijo@users.noreply.github.com>
2025-01-06 23:00:44 +0700

    เพิ่มฟังก์ชั่นค้นหาในหน้าแสดงรายวิชาและปรับปรุง UI โดยแยกส่วนค้นหาเป็นคอมโพเนนต์ใหม่

--- src/pages/subject/Subjects.jsx
+++ src/pages/subject/Subjects.jsx
@@ -4,6 +4,7 @@ import { HOSTNAME } from "../../config.js";
 import axios from "axios";
 import { Link, useLocation } from "react-router-dom";
 import AlertSuccess from "../../components/alert/success.jsx";
+import Search from "../../components/search.jsx";
 
 function Subjects() {
   const [subjects, setSubjects] = useState(null);
@@ -55,40 +56,7 @@ function Subjects() {
           เพิ่มวิชา
         </Link>
       </div>
-      <div className="mt-5 flex justify-end">
-        <div className="relative">
-          <label htmlFor="Search" className="sr-only">
-            Search
-          </label>
-          <input
-            type="text"
-            id="Search"
-            placeholder="รหัสวิชา หรือ ชื่อวิชา"
-            className="w-full rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
-            <button type="button" className="text-gray-600 hover:text-gray-700">
-              <span className="sr-only">Search</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-4"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                />
-              </svg>
-            </button>
-          </span>
-        </div>
-      </div>
+        <Search placeholder={"รหัสวิชา หรือชื่อวิชา"} search={search} setSearch={setSearch} />
       <div className="mt-5">
         {subjects ? (
           <SubjectList subjects={subjects} subjectsPerPage={50} />
