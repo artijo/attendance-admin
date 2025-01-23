45d8587bfa49d4d68c3225bb226808c463205c87
ApichaiTi <81065469+artijo@users.noreply.github.com>
2025-01-23 17:16:29 +0700

    Update CreateCalendar.jsx

--- src/pages/calendarmanage_new/CreateCalendar.jsx
+++ src/pages/calendarmanage_new/CreateCalendar.jsx
@@ -163,7 +163,7 @@ function CreateCalendar(){
                             className="block w-fit ml-auto text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
                             
                     >
-                            เพิ่มปีการศึกษา
+                            เพิ่มปฎิทินการเรียน
                     </button>
                 </form>          
             </div>
