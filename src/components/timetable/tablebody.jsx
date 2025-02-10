47a0e1efe445be2780bf38c920b633011572c2e4
Flower Garden <125670874+PeeraponWWW@users.noreply.github.com>
2025-02-10 20:27:35 +0700

    Update tablebody.jsx

--- src/components/timetable/tablebody.jsx
+++ src/components/timetable/tablebody.jsx
@@ -28,25 +28,6 @@ export const Tablebody = ({ arraySubject, day, timeStudyList, classroomId }) => 
         deleteTimetable(timetableId);
     }
 
-
-    const spanDay = (day) => {
-        switch (day) {
-            case 1: // จันทร์ (เหลือง)
-                return "cursor-pointer bg-yellow-200 text-yellow-600 px-5 py-[2px] rounded-sm";
-            case 2: // อังคาร (ชมพู)
-                return "cursor-pointer bg-pink-200 text-pink-600 px-5 py-[2px] rounded-sm";
-            case 3: // พุธ (เขียว)
-                return "cursor-pointer bg-green-200 text-green-600 px-5 py-[2px] rounded-sm";
-            case 4: // พฤหัสบดี (ส้ม)
-                return "cursor-pointer bg-orange-200 text-orange-600 px-5 py-[2px] rounded-sm";
-            case 5: // ศุกร์ (ฟ้า)
-                return "cursor-pointer bg-blue-200 text-blue-600 px-5 py-[2px] rounded-sm";
-            default:
-                return "cursor-pointer bg-gray-200 text-gray-600 px-5 py-[2px] rounded-sm"; // ค่าเริ่มต้น (สีเทา)
-        }
-    };
-    
-
     return (
         <tr className=''>
             <td className="whitespace-nowrap px-4 py-2 font-medium">
