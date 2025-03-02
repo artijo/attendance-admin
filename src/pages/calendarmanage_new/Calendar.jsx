7dec2c66b86c944d93798a83de07ec0dc7ab0e4d
Flower Garden <125670874+PeeraponWWW@users.noreply.github.com>
2025-03-02 18:18:14 +0700

    แก้ UI ตารางปฎิทิน

--- src/pages/calendarmanage_new/Calendar.jsx
+++ src/pages/calendarmanage_new/Calendar.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 function Calendar(){
     const [academicYearTermList, setAcademicYearTermList] = useState([]);
     const [selectedAcademicYearTerm, setSelectedAcademicYearTerm] = useState("");
-
+    const [selectedClassLevel, setSelectedClassLevel] = useState("1");
 
     const fetchAcademicYearTermList = async () => {
         try {
@@ -27,28 +27,48 @@ function Calendar(){
 
     return(
         <div className="container mx-auto">
-            <h1 className="mb-4">ปฎิทินการเรียน</h1>
+            <h1>ปฎิทินการเรียน</h1>
             <div>
-                <div className="mb-2 flex justify-between items-end">
-                    <div>
-                        <label className="text-xs block font-medium text-gray-700">
-                                ปีการศึกษาและเทอม
-                                <span className="ml-2 text-blue-600 underline"> 
-                                    <Link to="/terms/create">เพิ่มปีการศึกษา</Link></span>
-                                </label>
-                        <select 
-                            name="academicyear_term" 
-                            onChange={(e) => {setSelectedAcademicYearTerm(e.target.value) 
-                                console.log(e.target.value)}}
-                            className="mt-1 w-fit px-2 h-8 rounded-md border-gray-200 shadow-sm sm:text-sm border"
-                        >
-                            {
-                                academicYearTermList.map((academicYearTerm) => (
-                                    <option key={academicYearTerm.termId} value={academicYearTerm.termId}>ปีการศึกษา{academicYearTerm.academicYear} เทอม {academicYearTerm.semester}</option>
-                                ))
-                            }
-                        </select>
+                <div className="mt-2 mb-2 flex justify-between items-end">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div>
+                            <label className="text-xs block font-medium text-gray-700">
+                                    ปีการศึกษาและเทอม
+                                    <span className="ml-2 text-blue-600 underline"> 
+                                        <Link to="/terms/create">เพิ่มปีการศึกษา</Link></span>
+                                    </label>
+                            <select 
+                                name="academicyear_term" 
+                                onChange={(e) => {setSelectedAcademicYearTerm(e.target.value) 
+                                    console.log(e.target.value)}}
+                                className="mt-1 w-fit px-2 h-8 rounded-md border-gray-200 shadow-sm sm:text-sm border"
+                            >
+                                {
+                                    academicYearTermList.map((academicYearTerm) => (
+                                        <option key={academicYearTerm.termId} value={academicYearTerm.termId}>ปีการศึกษา{academicYearTerm.academicYear} เทอม {academicYearTerm.semester}</option>
+                                    ))
+                                }
+                            </select>
+                        </div>
+                        <div>
+                            <label className="text-xs block font-medium text-gray-700">
+                                ชั้นมัธยมศึกษาปีที่
+                            </label>
+                            <select 
+                                name="level" 
+                                onChange={(e) => setSelectedClassLevel(e.target.value)}
+                                className="mt-1 w-fit px-2 h-8 rounded-md border-gray-200 shadow-sm sm:text-sm border"
+                            >
+                                <option value="1">ม.1</option>
+                                <option value="2">ม.2</option>
+                                <option value="3">ม.3</option>
+                                <option value="4">ม.4</option>
+                                <option value="5">ม.5</option>
+                                <option value="6">ม.6</option>
+                            </select>
+                        </div>
                     </div>
+                    
                     <Link to="/calendar/create">
                         <div  className="inline-flex justify-center items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                             <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -63,7 +83,7 @@ function Calendar(){
                 <div id="classroomList">
                         {
                             selectedAcademicYearTerm !== "" && (
-                                <RoomList academicYearTermId={selectedAcademicYearTerm}/>
+                                <RoomList academicYearTermId={selectedAcademicYearTerm} selectedClassLevel={selectedClassLevel}/>
                             )
                         }
                 </div>
