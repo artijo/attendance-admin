bcab3a1e836a1bf0fa9b28f21a241ebdcf12cbd5
Flower Garden <125670874+PeeraponWWW@users.noreply.github.com>
2025-03-02 18:43:02 +0700

    แก้ icon calendar

--- src/components/calendar_new/roomlist.jsx
+++ src/components/calendar_new/roomlist.jsx
@@ -72,7 +72,7 @@ export const  RoomList = ({ academicYearTermId, selectedClassLevel } ) => {
                                                     className="flex items-center gap-2  p-3 text-blue-500 hover:bg-gray-50 focus:relative"
                                                     state={{classroomId:classroom.classId,classroomInfo:classroom}}
                                                 >
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                                                       <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
                                                     </svg>
 
@@ -83,7 +83,7 @@ export const  RoomList = ({ academicYearTermId, selectedClassLevel } ) => {
                                                     className="flex items-center gap-2  p-3 text-blue-500 hover:bg-gray-50 focus:relative"    
                                                     state={{classroomId:classroom.classId,classroomInfo:classroom}}
                                                 >
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                                                       <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819" />
                                                     </svg>
 
@@ -94,7 +94,7 @@ export const  RoomList = ({ academicYearTermId, selectedClassLevel } ) => {
                                                     className="flex items-center gap-2  p-3 text-red-500 hover:bg-gray-50 focus:relative"
                                                     onClick={() => handelDeleteStudingTime(classroom.classId)}
                                                 >
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                     </svg>
                                                     ลบปฎิทิน
