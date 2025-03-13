aa0337f2cf9c74835d42851cc98912175787e6bb
Kuy gu dum dev <125670874+PeeraponWWW@users.noreply.github.com>
2025-03-14 00:45:38 +0700

    แก้ button รายละเอียดการเข้าเรียนตามห้องเรียน

--- src/pages/attendence/AttendenceSummaryByClassroom.jsx
+++ src/pages/attendence/AttendenceSummaryByClassroom.jsx
@@ -25,7 +25,7 @@ function AttendenceSummaryByClassroom({classroomId}){
                     <Link state={{ classroomId: classroomId, studentList: studentList }} to={'/attendances/details/byclassroom'}>
                         <button 
                             
-                            className="inline-block p-3 text-blue-600 hover:bg-gray-50 focus:relative"
+                            className="inline-flex p-3 text-blue-600 hover:bg-gray-50 focus:relative"
                         >
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z" />
