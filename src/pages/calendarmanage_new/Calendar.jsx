da6b30cc576832014b1a9391406b187dd29deaa8
Peerapon Loa-suangkoon <125670874+PeeraponWWW@users.noreply.github.com>
2025-03-21 15:51:29 +0700

    ลบหน้าที่ไม่ได้ใช้แล้ว

--- src/pages/calendarmanage_new/Calendar.jsx
+++ src/pages/calendarmanage_new/Calendar.jsx
@@ -68,15 +68,6 @@ function Calendar(){
                             </select>
                         </div>
                     </div>
-                    
-                    <Link to="/calendar/create">
-                        <div  className="inline-flex justify-center items-center px-4 py-2 mt-5 border border-transparent shadow-sm text-sm font-medium rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                            <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                            </svg>
-                            เพิ่มปฎิทินการเรียน
-                        </div>
-                    </Link>
                 </div>
                 <div id="classroomList">
                         {
