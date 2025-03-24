df2827b8eb02a625387021e1e8d6375e9b778747
Kuy gu dum dev <125670874+PeeraponWWW@users.noreply.github.com>
2025-03-24 21:24:03 +0700

    comment ฟีลหีวหน้าห้องเนื่องจากยังไม่ได้ทำส่วนของหัวหน้าห้อง

--- src/pages/classroom/CreateForm.jsx
+++ src/pages/classroom/CreateForm.jsx
@@ -434,7 +434,7 @@ function CreateClassroom() {
                                                 />
                                             </div>
                                             
-                                            <div className="space-y-2">
+                                            {/* <div className="space-y-2">
                                                 <label htmlFor={`Leader_${index}`} className="text-sm font-medium text-text-color font-body flex items-center">
                                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -451,7 +451,7 @@ function CreateClassroom() {
                                                     placeholder="เลือกหัวหน้าห้อง..."
                                                     noOptionsMessage={() => "ไม่พบข้อมูล"}
                                                 />
-                                            </div>
+                                            </div> */}
                                         </div>
                                     </div>
                                 ))}
--- src/pages/classroom/EditForm.jsx
+++ src/pages/classroom/EditForm.jsx
@@ -292,7 +292,7 @@ function EditClassroom() {
                             />
                         </div>
 
-                        <div className="space-y-2">
+                        {/* <div className="space-y-2">
                             <label htmlFor="Leader" className="text-sm font-medium text-text-color font-body flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -310,7 +310,7 @@ function EditClassroom() {
                                 placeholder="เลือกหัวหน้าห้อง..."
                                 noOptionsMessage={() => "ไม่พบข้อมูล"}
                             />
-                        </div>
+                        </div> */}
 
                         <div className="sm:col-span-2 flex justify-between items-center pt-6 border-t border-gray-100 mt-4">
                             <Link 
