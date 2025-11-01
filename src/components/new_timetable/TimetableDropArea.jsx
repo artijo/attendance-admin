849c7155dd1da9bc1f97a61a6613492528f84751
Peerapon Loa-suangkoon <125670874+PeeraponWWW@users.noreply.github.com>
2025-11-01 12:52:04 +0700

    imporve: drop area timetable ui

--- src/components/new_timetable/TimetableDropArea.jsx
+++ src/components/new_timetable/TimetableDropArea.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import { formatDayOfWeeks } from "../../helper";
 
-export const TimetableDropArea = ({schedule,weekday,onDrop}) => {
+export const TimetableDropArea = ({ schedule, weekday, onDrop }) => {
     return (
-        <div 
-            className={`min-w-52 h-32 `}
+        <div
+            className={`min-w-52 h-32 p-2`}
             onDrop={() => onDrop(weekday, schedule)}
-            onDragOver={e =>e.preventDefault()}
-        ></div>
+            onDragOver={e => e.preventDefault()}
+        >
+            <p className="text-sm italic text-gray-600">
+                {formatDayOfWeeks(weekday)} {schedule.timetableformate}
+            </p>
+            <p className="text-xs text-gray-400 italic mt-1">
+                วางรายวิชาที่นี่
+            </p>
+        </div>
     );
 };
